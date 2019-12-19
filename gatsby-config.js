c6ce86db1564463a250fd27a961bdb3afc34a4ef
Allan King <AllanKing27@gmail.com>
2019-12-19 08:38:46 +0000

    Create production only context for ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   siteMetadata: {
     title: `proto site`,
   },
@@ -12,33 +12,20 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-27643243-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
-      },
-    },
     `gatsby-transformer-remark`
   ],
 }
+
+if (process.env.CONTEXT === "production") {
+  const googleAnalyticsConfig = {
+    resolve: "gatsby-plugin-google-analytics",
+    options: {
+      trackingId: "UA-27643243-1",
+      anonymize: true,
+      respectDNT: true,
+    }
+  };
+  config.plugins.push(googleAnalyticsConfig);
+}
+
+module.exports = config;
