dfe66bc086d904b8d819f66d5e15ee5f1b938870
wellhairy <AllanKing27@gmail.com>
2020-01-17 14:56:05 +0000

    Add basic PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,27 @@ const config = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
-    `gatsby-transformer-sharp`, 
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Wellhairy design - Allan King`,
+        short_name: `wellhairy-design`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/index/`, `/gallery/*`],
+      }
+    },
   ],
 }
 
