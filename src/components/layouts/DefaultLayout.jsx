268016d485f5a277235fcd487d55220e6abdbd69
Allan King <AllanKing27@gmail.com>
2020-01-19 16:49:34 +0000

    Remove font's from the Helemt in layout

--- src/components/layouts/DefaultLayout.jsx
+++ src/components/layouts/DefaultLayout.jsx
@@ -8,10 +8,6 @@ export default ({ children }) => (
     <Helmet>
       <meta charSet="utf-8" />
       <title>Wellhairy design - Allan King</title>
-      {/* <link rel="icon" href="favicon.png" /> */}
-      <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"></link>
-      <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet"></link>
-      <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One" rel="stylesheet"></link>
     </Helmet>
 
     <div className="container">
